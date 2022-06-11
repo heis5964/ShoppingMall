53cf0c3c3a895f8deb0a4fa309ef76332c2a97cf
suwat <heis596409@gmail.com>
2022-06-11 21:19:31 +0700

    update api

--- api/insertUser.php
+++ api/insertUser.php
@@ -30,7 +30,7 @@ if (isset($_GET)) {
 
 		
 							
-		$sql = "INSERT INTO `user`(`id`, `name`, `type`, `address`, `phone`, `user`, `password`, `avatar`, `lat`, `lng`) VALUES (Null,'$name','$type','$address','$phone','$user','$password','$avatar','$lat','$lng'')";
+		$sql = "INSERT INTO `user`(`id`, `name`, `type`, `address`, `phone`, `user`, `password`, `avatar`, `lat`, `lng`) VALUES (Null,'$name','$type','$address','$phone','$user','$password','$avatar','$lat','$lng')";
 
 		$result = mysqli_query($link, $sql);
 
